731178082df90dc6b76e71908dde4cf63c869235
garam <garam-park@naver.com>
2018-11-03 04:38:05 +0900

    예제2 퀵소트 구현

---
+++ ch01_recursive/ex02.py
@@ -0,0 +1,19 @@
+def quickSort(array):
+    
+    if len(array)<=1 :
+        return array
+
+    pivot = array[0]
+    
+    left = [x for x in array[1:] if pivot > x]
+    right = [x for x in array[1:] if pivot <= x]
+
+    return quickSort(left) + [pivot] + quickSort(right)
+
+def main():
+    line = [int(x) for x in input().split()]
+
+    print(*quickSort(line))
+
+if __name__ == "__main__":
+    main()
