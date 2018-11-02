eedc4fa44874200c5f46725bac93f9a2feb00a00
garam <garam-park@naver.com>
2018-11-03 04:46:16 +0900

    예제 3  올바른 괄호 판다

---
+++ ch01_recursive/ex03.py
@@ -0,0 +1,30 @@
+def checkParen(p):
+
+    if len(p) == 0:
+        return "YES"
+
+    if len(p) == 2:
+        if p == "()":
+            return "YES"
+        else:
+            return "NO"
+    
+    for i in range(0,len(p)-1):
+        if p[i] == "(" and p[i+1] == ')'
+            tmp = p[:i] + p[i+2:]
+            return checkParen(tmp)
+    
+    return "NO"
+
+def main():
+    '''
+    Do not change this code
+    '''
+
+    x = input()
+    print(checkParen(x))
+
+if __name__ == "__main__":
+    main()
+
+
